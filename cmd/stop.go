70f796dae45fd099eb8646b5f1a2d2c3dd7ba47d
Jesse Vincent <jesse@fsck.com>
2025-10-23 03:19:24 +0000

    feat: implement cage stop command
    - Add stop command to stop and remove containers
    - Require --worktree flag to identify container
    - Use docker stop followed by docker rm
    - Provide user feedback on stop/remove progress

--- cmd/stop.go
+++ cmd/stop.go
@@ -1,6 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/jessedrelick/cage/pkg/container"
+	"github.com/jessedrelick/cage/pkg/docker"
 	"github.com/spf13/cobra"
 )
 
@@ -13,9 +19,52 @@ var stopCmd = &cobra.Command{
 	Use:   "stop [flags]",
 	Short: "Stop container",
 	Long:  `Stop the container for the specified project/worktree.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: implement
-		cmd.Println("stop command not yet implemented")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// Determine working directory
+		workDir := stopPath
+		if workDir == "" {
+			var err error
+			workDir, err = os.Getwd()
+			if err != nil {
+				return fmt.Errorf("failed to get working directory: %w", err)
+			}
+		}
+
+		workDir, err := filepath.Abs(workDir)
+		if err != nil {
+			return fmt.Errorf("failed to resolve path: %w", err)
+		}
+
+		// Determine worktree name
+		worktreeName := stopWorktree
+		if worktreeName == "" {
+			return fmt.Errorf("--worktree flag is required for stop")
+		}
+
+		// Generate container name
+		containerName := container.GenerateContainerName(workDir, worktreeName)
+
+		// Initialize Docker client
+		dockerClient, err := docker.NewClient(false)
+		if err != nil {
+			return fmt.Errorf("failed to initialize docker: %w", err)
+		}
+
+		// Stop container
+		fmt.Printf("Stopping container %s...\n", containerName)
+		_, err = dockerClient.Run("stop", containerName)
+		if err != nil {
+			return fmt.Errorf("failed to stop container: %w", err)
+		}
+
+		// Remove container
+		_, err = dockerClient.Run("rm", containerName)
+		if err != nil {
+			return fmt.Errorf("failed to remove container: %w", err)
+		}
+
+		fmt.Printf("Container %s stopped and removed\n", containerName)
+		return nil
 	},
 }
 
